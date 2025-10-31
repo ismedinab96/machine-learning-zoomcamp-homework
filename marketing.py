1360a820b93e03425d9e0b17d2b8e4bd711d811b
ismedinab.96 <128383796+ismedinab96@users.noreply.github.com>
2025-10-31 15:57:06 +0000

    homework 6

---
+++ marketing.py
@@ -0,0 +1,32 @@
+import requests
+
+url = 'http://localhost:9696/predict'
+
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "month-to-month",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 1,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85,
+    
+}
+response = requests.post(url, json=customer)
+churn = response.json()
+
+print((f'Response = {churn}'))
+message = 'send promo' if churn['churn'] >= 0.5 else 'do not do anything'
+print(message)
